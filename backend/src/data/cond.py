715b0f924b3f0009649fc09d0a78549e5306f412
zhdbk3 <zhdbk3@outlook.com>
2025-07-21 19:47:09 +0800

    完成原始形式解析器对向量点乘和角度制的支持

--- backend/src/data/cond.py
+++ backend/src/data/cond.py
@@ -7,17 +7,49 @@ from .math_obj import MathObj
 from vec_parse_utils import mark_vec_coord
 
 
+def map_vec_coord(expr: str) -> tuple[str, dict[str, str]]:
+    """
+    将向量的坐标表示（已标记为 ``Matrix``）映射到一个变量上，以便 ``sympy.sympify`` 将它直接当作一个普通符号解析
+    （这么做是因为即使设置了 ``sympy.sympify(evaluate=False)`` 也无法阻止向量“数乘”的执行）
+    :param expr: 原始字符串表达式
+    :return: 两个值：
+             - 替换后的字符串表达式，可以直接喂给 ``sympy.sympify`` 解析
+             - 映射表，键为向量坐标映射到的变量名，值为它自己的 LaTeX
+    """
+    mapping = {}
+    while True:
+        left_index = expr.find('Matrix([')
+        if left_index != -1:
+            right_index = expr.find('])') + 2
+            vec_coord = expr[left_index:right_index]
+            # 用哈希值确保映射的唯一性，平方消除负号
+            # 末尾有字母，数字不会被 ``sympy.sympify` 变成下标
+            alias = f'vec{hash(vec_coord) ** 2}coord'
+            expr = expr.replace(vec_coord, alias)
+            mapping[alias] = latex(sympify(vec_coord, evaluate=False), mul_symbol='dot')
+        else:
+            return expr, mapping
+
+
 def to_raw_latex(expr: str) -> str:
     """
     生成出用户原始输入的表达式的 LaTeX
     只能是单个表达式
     """
-    expr = mark_vec_coord(expr)
+    expr = (mark_vec_coord(expr)
+            .replace('deg', '* gcdeg')  # SymPy 内有个函数就叫 ``deg``，故在此做区分
+            .replace('dot', '*'))
 
-    # TODO: 支持向量点乘、角度制
-    expr = latex(sympify(expr))
+    expr, mapping = map_vec_coord(expr)
+
+    expr = latex(sympify(expr, evaluate=False), mul_symbol='dot')
+
+    for alias, vec_coord_latex in mapping.items():
+        expr = expr.replace(alias, vec_coord_latex)
 
     rules = [
+        # ·gcdeg -> °
+        (r'\\cdot\s+gcdeg', r'^{\\circ}'),
         #          ->
         # vecAB -> AB
         (r'\bvec([A-Z]{2})\b', r'\\overrightarrow{\1}'),
@@ -26,7 +58,7 @@ def to_raw_latex(expr: str) -> str:
         # angABC -> ∠ABC
         (r'\bang([A-Z]{3})\b', r'\\angle \1'),
         # 删除多余点号
-        (r'(?:(?<![A-Z])\s+\\cdot\s+)|(?:\s+\\cdot\s+(?![A-Z]))', '')
+        (r'[a-z0-9]\s*(\\cdot)\s*(?:[a-zA-Z]|\\overrightarrow)', '')
     ]
     for pattern, repl in rules:
         expr = re.sub(pattern, repl, expr)
