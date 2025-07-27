2b1b1b6c120a0a054fe94a8fc72e6ecdd5f13624
zhdbk3 <zhdbk3@outlook.com>
2025-07-27 18:35:52 +0800

    优化后端日志：报错记录详细调用栈，启动时记录版本

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,11 @@
 import webview
 
 from api import api
+from logger import backend_logger
 
 __version__ = '2.1.0'
+
+backend_logger.info(f'几何计算器，启动！版本：{__version__}')
 
 window = webview.create_window('几何计算器', 'ui/index.html', js_api=api, maximized=True)
 webview.start()
